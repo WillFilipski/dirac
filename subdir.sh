fe2af3eb03ed17b7815de850082e2bd8fd31a03c
Will F <48337016+WillFilipski@users.noreply.github.com>
2022-09-24 17:13:28 -0600

    Create subdir.sh

---
+++ subdir.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+for ii in *.xyz ; do
+    i=${ii%.sub
+    }
+
+    cat > ${i}.sub <<EOF
+#! /bin/bash
+#SBATCH -t 1-00:00
+#SBATCH -J ${i}
+#SBATCH -o ${i}.out
+#SBATCH -e ${i}.err
+#SBATCH -N 1
+#SBATCH -n 32
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --account=def-dilabiog-ac
+
+module load StdEnv intel intelmpi
+module load dirac
+
+pam --mol=${i}.xyz --input=hf.inp
+
+EOF
+done
